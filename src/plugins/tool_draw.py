d02fa0ab628e13f2f988ac289546a5a1fc69b155
Christian Moritz <chrmoritz@gmail.com>
2015-04-04 14:03:44 +0200

    draw tool: implement area draw (between 2 voxels) while holding shift

--- src/plugins/tool_draw.py
+++ src/plugins/tool_draw.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from PySide import QtGui
+from PySide import QtGui, QtCore
 from tool import Tool, EventData, MouseButtons, KeyModifiers, Face
 from plugin_api import register_plugin
 
@@ -31,26 +31,43 @@ class DrawingTool(Tool):
         self.action.setShortcut(QtGui.QKeySequence("Ctrl+1"))
         # Register the tool
         self.api.register_tool(self, True)
+        # Area tool helper
+        self.first_voxel = None
 
     # Tries to plot a new voxel at target location.
     # param choosen_target: The place where the new voxel should be inserted.
     # returns A Target object indicating the actual place where the voxel were
     # inserted. Returns None when no insertion was made.
-    def _draw_voxel(self, target):
+    def _draw_voxel(self, target, shift_down, erase):
         # Works out where exactly the new voxel goes. It can collide with an existing voxel or with the bottom of the 'y' plane,
         #in which case, pos will be different than None.
-        pos = target.get_neighbour()
-        if pos:
-            target.world_x = pos[0]
-            target.world_y = pos[1]
-            target.world_z = pos[2]
-        # Tries to set the voxel on the matrix and then returns the Target
-        # with it's coordinates, if it exists.
-        if( target.voxels.set(target.world_x, target.world_y, target.world_z,
-                              self.colour) ):
-            return target
+        color = self.colour
+        if erase:
+            color = 0
         else:
-            return None
+            pos = target.get_neighbour()
+            if pos:
+                target.world_x = pos[0]
+                target.world_y = pos[1]
+                target.world_z = pos[2]
+
+        if shift_down and self.first_voxel == None:
+            self.first_voxel = (target.world_x, target.world_y, target.world_z)
+        elif self.first_voxel == None:
+            if target.voxels.set(target.world_x, target.world_y, target.world_z, color):
+                return target
+            else:
+                return None
+        else:
+            dx = target.world_x < self.first_voxel[0] and 1 or -1
+            for x in xrange(target.world_x, self.first_voxel[0] + dx, dx):
+                dy = target.world_y < self.first_voxel[1] and 1 or -1
+                for y in xrange(target.world_y, self.first_voxel[1] + dy, dy):
+                    dz = target.world_z < self.first_voxel[2] and 1 or -1
+                    for z in xrange(target.world_z, self.first_voxel[2] + dz, dz):
+                        target.voxels.set(x, y, z, color)
+            self.first_voxel = None
+        return None
 
     def _get_valid_sequence_faces(self, face):
         if( face in Face.COLLIDABLE_FACES_PLANE_X ):
@@ -64,10 +81,8 @@ class DrawingTool(Tool):
 
     # Draw a new voxel next to the targeted face
     def on_mouse_click(self, data):
-        if data.mouse_button == MouseButtons.LEFT:
-            self._first_target = self._draw_voxel(data)
-        elif data.mouse_button == MouseButtons.RIGHT:
-            data.voxels.set(data.world_x, data.world_y, data.world_z, 0)
+        shift_down = not not (data.key_modifiers & QtCore.Qt.KeyboardModifier.ShiftModifier)
+        self._first_target = self._draw_voxel(data, shift_down, data.mouse_button == MouseButtons.RIGHT)
 
     # Start a drag
     def on_drag_start(self, data):
@@ -81,6 +96,6 @@ class DrawingTool(Tool):
         valid_faces = self._get_valid_sequence_faces(self._first_target.face)
         if( ( not valid_faces ) or ( data.face not in valid_faces ) ):
             return
-        self._draw_voxel(data)
+        self._draw_voxel(data, False, False)
 
 register_plugin(DrawingTool, "Drawing Tool", "1.0")
